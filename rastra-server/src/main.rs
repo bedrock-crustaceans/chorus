94d985976d6a9417a3406e4663c43b494c7092ea
xRookieFight <me@xrookiefight.com>
2026-02-27 00:09:00 +0300

    Initial work for project

---
+++ rastra-server/src/main.rs
@@ -0,0 +1,21 @@
+use log::debug;
+use rastra_server::server::Server;
+use rastra_server::utils::Logger;
+
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let config = rastra_config::setup_config();
+
+    Logger::setup_logger(config.log_to_file, &config.logs_directory);
+
+    debug!("Tokio runtime initialized");
+
+    let server = Server {
+        config,
+    };
+
+    server.run().await.expect("Server crashed");
+
+    Ok(())
+}
---
+++ rastra-server/src/server.rs
@@ -0,0 +1,48 @@
+use bedrockrs::proto::connection::Connection;
+use bedrockrs::proto::listener::Listener;
+use bedrockrs::proto::v786;
+use log::{debug, info};
+use rastra_config::RAstraConfig;
+
+pub struct Server {
+    pub config: RAstraConfig
+}
+
+impl Server {
+    pub async fn run(&self) -> anyhow::Result<()>{
+        let config = &self.config;
+
+        let ip = &config.ip;
+        let port = config.port;
+
+        info!("Server starting on {}:{}", ip, port);
+
+        let mut listener = Listener::new_raknet(
+            config.motd.to_string(),
+            config.sub_motd.to_string(),
+            "1.21.70".to_string(),
+            v786::info::PROTOCOL_VERSION,
+            config.max_players,
+            0, // TODO
+            format!("{}:{}", ip, port).parse().unwrap(),
+            false,
+        )
+            .await?;
+
+        listener.start().await?;
+
+        info!("Server started successfully!");
+
+        loop {
+            let conn = listener.accept().await?;
+
+            tokio::spawn(async move {
+                handle_conn(conn).await;
+            });
+        }
+
+        async fn handle_conn(conn: Connection) {
+            debug!("Got connection from {:?}", conn.get_socket_addr().to_string());
+        }
+    }
+}
