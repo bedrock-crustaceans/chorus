13a272892a2b4b31a4435d317bc0130774116239
xRookieFight <me@xrookiefight.com>
2026-03-24 22:28:59 +0300

    chore: Add some macros

--- rastra-api/src/macros.rs
+++ rastra-api/src/macros.rs
@@ -10,3 +10,54 @@ macro_rules! modules {
         )*
     };
 }
+
+#[macro_export]
+macro_rules! singleton {
+    ($name:ident : $ty:ty) => {
+        static mut $name: Option<$ty> = None;
+
+        impl $ty {
+            pub fn instance() -> &'static mut $ty {
+                unsafe {
+                    $name.as_mut().expect("Singleton not initialized")
+                }
+            }
+
+            pub fn init(value: $ty) {
+                unsafe {
+                    $name = Some(value);
+                }
+            }
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! reexports {
+    ($($path:path),* $(,)?) => {
+        $(
+            pub use $path;
+        )*
+    };
+}
+
+#[macro_export]
+macro_rules! boxed {
+    ($value:expr) => {
+        Box::new($value)
+    };
+}
+
+#[macro_export]
+macro_rules! arc {
+    ($value:expr) => {
+        std::sync::Arc::new($value)
+    };
+}
+
+#[macro_export]
+macro_rules! lock {
+    ($value:expr) => {
+        $value.lock().unwrap()
+    };
+}
